0eff2b2ddc8dd3273edd59bd243ad7e412cd9ff5
v-nebe <katya.shauneva@gmail.com>
2023-01-30 18:59:02 +0300

    task completed

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -6,8 +6,7 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-        String phrase = "writing the same code doesn't have much impact, and it's also time consuming\n";
-        String repeated = phrase.repeat(20);
-        System.out.println(repeated);
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming\n");
+
     }
 }
