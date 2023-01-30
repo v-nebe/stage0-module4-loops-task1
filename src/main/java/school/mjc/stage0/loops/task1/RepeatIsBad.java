483b4599a3fc8b1c8da2c58f2897b060ebe81886
v-nebe <katya.shauneva@gmail.com>
2023-01-30 18:44:04 +0300

    task completed

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,10 +1,13 @@
 package school.mjc.stage0.loops.task1;
 
+import java.util.stream.IntStream;
+
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-        for (int i =0; i<20; i++)
-            System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+        IntStream.range(0, 20)
+                .mapToObj(x -> "writing the same code doesn't have much impact, and it's also time consuming")
+                .forEach(System.out::println);
     }
 }
