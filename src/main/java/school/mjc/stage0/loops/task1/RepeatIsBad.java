6a17dcb7f4aa46111643b99d524bd262d9f80948
v-nebe <katya.shauneva@gmail.com>
2023-01-30 18:37:02 +0300

    task completed

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,7 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        for (int i =0; i<20; i++)
+            System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
     }
 }
